e52aef344877d0f92d48ad31e7bb2d2036983292
irvandyIN <rendrairvandy@gmail.com>
2023-11-25 16:27:17 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ph-dfrobot-sen0249-esp32-platformio
+This repository contains coding for reading the DFRobot SEN0249 pH sensor using FreeRTOS
 # ph-dfrobot-sen0249-esp32-platformio
--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,127 @@
 #include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-#define PHSensorPin  34    //dissolved oxygen sensor analog output pin to arduino mainboard
-#define VREF 3.3    //for arduino uno, the ADC reference is the AVCC, that is 5.0V(TYP)
-#define OFFSET 0.00  //zero drift compensation
+#define PHSensorPin 34
+#define VREF 3.3
+#define OFFSET 0.00
+#define SCOUNT 30
 
-#define SCOUNT  30           // sum of sample point
-int analogBuffer[SCOUNT];    //store the analog value in the array, readed from ADC
+int analogBuffer[SCOUNT];
 int analogBufferTemp[SCOUNT];
-int analogBufferIndex = 0,copyIndex = 0;
+int analogBufferIndex = 0, copyIndex = 0;
+float averageVoltage, phValue;
 
-float averageVoltage,phValue;
+SemaphoreHandle_t xMutex;
 
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(PHSensorPin,INPUT);
-  pinMode(2, OUTPUT); // LED bawaan ESP32 terhubung ke GPIO 2
-  // put your setup code here, to run once:
+void readAnalogTask(void *pvParameters)
+{
+  static unsigned long analogSampleTimepoint = millis();
+  while (1)
+  {
+    if (millis() - analogSampleTimepoint > 30U)
+    {
+      analogSampleTimepoint = millis();
+      analogBuffer[analogBufferIndex] = analogRead(PHSensorPin);
+      analogBufferIndex++;
+      if (analogBufferIndex == SCOUNT)
+        analogBufferIndex = 0;
+    }
+    vTaskDelay(10); // Adjust the delay based on the desired task frequency
+  }
 }
 
 int getMedianNum(int bArray[], int iFilterLen)
 {
-      int bTab[iFilterLen];
-      for (byte i = 0; i<iFilterLen; i++)
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++)
+  {
+    bTab[i] = bArray[i];
+  }
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++)
+  {
+    for (i = 0; i < iFilterLen - j - 1; i++)
+    {
+      if (bTab[i] > bTab[i + 1])
       {
-      bTab[i] = bArray[i];
-      }
-      int i, j, bTemp;
-      for (j = 0; j < iFilterLen - 1; j++)
-      {
-      for (i = 0; i < iFilterLen - j - 1; i++)
-          {
-        if (bTab[i] > bTab[i + 1])
-            {
         bTemp = bTab[i];
-            bTab[i] = bTab[i + 1];
+        bTab[i] = bTab[i + 1];
         bTab[i + 1] = bTemp;
-         }
       }
-      }
-      if ((iFilterLen & 1) > 0)
+    }
+  }
+  if ((iFilterLen & 1) > 0)
     bTemp = bTab[(iFilterLen - 1) / 2];
-      else
+  else
     bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-      return bTemp;
+  return bTemp;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-   static unsigned long analogSampleTimepoint = millis();
-   if(millis()-analogSampleTimepoint > 30U)     //every 30 milliseconds,read the analog value from the ADC
-   {
-     analogSampleTimepoint = millis();
-     analogBuffer[analogBufferIndex] = analogRead(PHSensorPin);    //read the analog value and store into the buffer
-     analogBufferIndex++;
-     if(analogBufferIndex == SCOUNT)
-         analogBufferIndex = 0;
-   }
-   static unsigned long printTimepoint = millis();
-   if(millis()-printTimepoint > 1000U)
-   {
+
+void printDataTask(void *pvParameters)
+{
+  static unsigned long printTimepoint = millis();
+  while (1)
+  {
+    if (millis() - printTimepoint > 1000U)
+    {
       printTimepoint = millis();
-      for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
+      if (xSemaphoreTake(xMutex, portMAX_DELAY))
       {
-        analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
+        for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
+        {
+          analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+        }
+        xSemaphoreGive(xMutex);
+
+        averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 4096.0;
+        phValue = 3.5 * averageVoltage + OFFSET;
+
+        Serial.print("Voltage:");
+        Serial.print(averageVoltage, 2);
+        Serial.print("   pH value:");
+        Serial.println(phValue, 2);
+
+        digitalWrite(2, HIGH);
+        delay(1000);
+        digitalWrite(2, LOW);
       }
-      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 4096.0; // read the value more stable by the median filtering algorithm
-      phValue = 3.5 * averageVoltage + OFFSET;
-      Serial.print("Voltage:");
-      Serial.print(averageVoltage,2);
-      Serial.print("   pH value:");
-      Serial.println(phValue,2);
-      digitalWrite(2, HIGH);
-      delay(1000); // tunggu 1 detik
-      digitalWrite(2, LOW); // matikan LED
-   }
+    }
+    vTaskDelay(10); // Adjust the delay based on the desired task frequency
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(PHSensorPin, INPUT);
+  pinMode(2, OUTPUT);
+
+  xMutex = xSemaphoreCreateMutex();
+
+  xTaskCreatePinnedToCore(
+      readAnalogTask,
+      "ReadAnalogTask",
+      10000,
+      NULL,
+      1,
+      NULL,
+      0);
+
+  xTaskCreatePinnedToCore(
+      printDataTask,
+      "PrintDataTask",
+      10000,
+      NULL,
+      1,
+      NULL,
+      1);
+}
+
+
+void loop()
+{
+  // Empty, as all functionality is in tasks
 }
